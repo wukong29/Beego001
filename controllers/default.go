295a2ea834bf698ab1f9c838076be383894180c7
wukong29 <2289476116@qq.com>
2020-09-22 19:28:48 +0800

    修改模板文件的网址和联系人

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@Email.com"
+	c.Data["Website"] = "www.baidu.com"
+	c.Data["Email"] = "2289476116@qq.com"
 	c.TplName = "index.tpl"
 }
